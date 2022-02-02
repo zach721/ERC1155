352b25e0f5cc5f8c0b0593ed94bf829f7699de8b
Zakarias <46316857+zach721@users.noreply.github.com>
2022-02-02 16:57:24 +0100

    Update pokemon.sol

--- pokemon.sol
+++ pokemon.sol
@@ -1,15 +1,40 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
 
-contract pokemon {
-    mapping(address=>mapping(uint=>Pokemon)) public mypokemon;
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+/// @custom:security-contact zach.saviour@gmail.com
+
+contract MyToken is ERC1155, Ownable, ERC1155Burnable {
+    uint256 nextid = 0;
+      mapping(address=>mapping(uint=>Pokemon)) public mypokemon;
 
 struct Pokemon{
     string _name;
     string _type;
 }
-
-function getpokemon(uint id,string memory name, string memory types) public {
-    mypokemon[msg.sender][id] = Pokemon(name,types);
+    constructor(string memory uri_) ERC1155(uri_) {}
+function mintpokemon(uint amount,string memory name_, string memory type_) public onlyOwner{
+    _mint(msg.sender,nextid,amount,"");
+    mypokemon[msg.sender][nextid++] = Pokemon(name_,type_);
+    nextid++;
 }
+    function setURI(string memory newuri) public onlyOwner {
+        _setURI(newuri);
+    }
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
 }
