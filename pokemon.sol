25f93ac273513b4d862b4222bb3aacd2e96caaaa
Zakarias <46316857+zach721@users.noreply.github.com>
2022-02-04 11:56:15 +0100

    Update pokemon.sol

--- pokemon.sol
+++ pokemon.sol
@@ -1,3 +1,4 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
@@ -8,7 +9,7 @@ import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
 
 contract MyToken is ERC1155, Ownable, ERC1155Burnable {
     uint256 nextid = 0;
-      mapping(address=>mapping(uint=>Pokemon)) public mypokemon;
+    mapping(uint=>Pokemon) public mypokemon;
 
 struct Pokemon{
     string _name;
@@ -17,24 +18,11 @@ struct Pokemon{
     constructor(string memory uri_) ERC1155(uri_) {}
 function mintpokemon(uint amount,string memory name_, string memory type_) public onlyOwner{
     _mint(msg.sender,nextid,amount,"");
-    mypokemon[msg.sender][nextid++] = Pokemon(name_,type_);
+    mypokemon[nextid++] = Pokemon(name_,type_);
     nextid++;
 }
-    function setURI(string memory newuri) public onlyOwner {
-        _setURI(newuri);
+   function gettokendetails(uint256 tokenid) public view returns(Pokemon memory){
+        return mypokemon[tokenid];
     }
-
-    function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mint(account, id, amount, data);
-    }
-
-    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
-        onlyOwner
-    {
-        _mintBatch(to, ids, amounts, data);
-    }
+    
 }
